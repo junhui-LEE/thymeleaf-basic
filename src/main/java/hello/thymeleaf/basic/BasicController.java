cd2ff4537f1f8618fcd0d5eab4b53590a9309424
junhui-LEE <junhee0386@naver.com>
2024-02-14 17:50:04 +0900

    연산

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -20,6 +20,13 @@ import java.util.Map;
 @RequestMapping("/basic")
 public class BasicController {
 
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
     @GetMapping("/literal")
     public String literal(Model model){
         model.addAttribute("data", "Spring!");
